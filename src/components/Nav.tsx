7faaf63a11c0a5c5fcf0bc14b20569378c1494d3
Ivan Batur <ivanbatur2g@gmail.com>
2024-01-08 10:51:30 +0100

    Navbar finished

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,25 +5,21 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthDispatch, useUserValue } from '../context/UserContextHooks';
 
 
 function Nav() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const user = useUserValue();
   const navigate = useNavigate()
   const dispatch = useAuthDispatch()
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -39,9 +35,7 @@ function Nav() {
        handleCloseUserMenu()
   }
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -49,9 +43,8 @@ function Nav() {
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+      <Container  maxWidth="xl">
+        <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
           <Typography
             variant="h6"
             noWrap
@@ -59,7 +52,7 @@ function Nav() {
             to={"/"}
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              display: { xs: 'flex', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
@@ -69,63 +62,14 @@ function Nav() {
           >
             HOME
           </Typography>
+                    
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+          <Box sx={{ flexGrow: 0,   }}>
+            <Tooltip title={user.isAuthenticated ? "Open menu": "Authenticate"}>
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" >{user.username[0]}</Avatar>
+                {user.isAuthenticated ? 
+                (<Avatar alt="Remy Sharp" >{user.username[0]}</Avatar>
+) : (<AccountCircleIcon sx={{color: "white"}} fontSize='large'/>)}
               </IconButton>
             </Tooltip>
             <Menu
@@ -157,7 +101,7 @@ function Nav() {
                 </MenuItem>]
               
                 
-              ): <MenuItem key={"Log In"}>
+              ): <MenuItem key={"Log In"} onClick={handleCloseUserMenu}>
                     <Typography textAlign={"center"} component={Link} to="/login">Log In</Typography>
                 </MenuItem>}
 
